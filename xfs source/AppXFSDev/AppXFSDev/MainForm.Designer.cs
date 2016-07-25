b76aab66952c977eceec5ab645f085110c520f95
Carlos Cp <carlosx_1302@hotmail.com>
2016-07-25 16:19:54 -0500

    Update add TreeView

--- xfs source/AppXFSDev/AppXFSDev/MainForm.Designer.cs
+++ xfs source/AppXFSDev/AppXFSDev/MainForm.Designer.cs
@@ -28,26 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Location = new System.Drawing.Point(7, 414);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(780, 248);
+            this.textBox1.Size = new System.Drawing.Size(780, 51);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(660, 292);
+            this.button1.Location = new System.Drawing.Point(594, 483);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(95, 37);
             this.button1.TabIndex = 1;
@@ -58,7 +61,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 394);
+            this.label1.Location = new System.Drawing.Point(23, 500);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 2;
@@ -68,17 +71,35 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Yu Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(720, 413);
+            this.label2.Location = new System.Drawing.Point(725, 500);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(67, 23);
             this.label2.TabIndex = 3;
             this.label2.Text = "CarlosX";
             // 
-            // Form1
+            // treeView1
+            // 
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(775, 380);
+            this.treeView1.StateImageList = this.imageList1;
+            this.treeView1.TabIndex = 4;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "icon.ico");
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(804, 444);
+            this.ClientSize = new System.Drawing.Size(804, 532);
+            this.Controls.Add(this.treeView1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
@@ -86,7 +107,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Edit XFS - CarlosX";
             this.ResumeLayout(false);
@@ -99,6 +120,8 @@
         public System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
 
--- xfs source/AppXFSDev/AppXFSDev/MainForm.cs
+++ xfs source/AppXFSDev/AppXFSDev/MainForm.cs
@@ -25,7 +25,7 @@ namespace AppXFSDev
             string _pathexe = System.Reflection.Assembly.GetExecutingAssembly().Location.ToString();
             string _direxe = Path.GetDirectoryName(_pathexe).ToString();
 
-            XfsFile xfs = new XfsFile(_direxe + "/Animations.xfs");
+            XfsFile xfs = new XfsFile(_direxe + "/DataSetup.xfs");
             StringBuilder strbuild = new StringBuilder();
             //strbuild.AppendFormat("Offset: 0x{0:X} zsize: {1} info_size: {2}", xfs.offset, xfs.zsize, xfs.info_size);
             //strbuild.AppendLine();
@@ -33,13 +33,34 @@ namespace AppXFSDev
             label1.Text = strbuild.ToString();
 
             StringBuilder strbuild2 = new StringBuilder();
+            List<string> pathx = new List<string>();
             foreach (XfsFileData fil in xfs.files)
             {
                 strbuild2.AppendFormat("fileName: '{0}' offset_file: 0x{1:X} compresed: {2} UCSize: {3} CSize: {4}", fil.fileName, fil.offset, fil.compresed, fil.UCSize, fil.CSize);
                 strbuild2.AppendLine();
+                pathx.Add(fil.fileName);
             }
             textBox1.Text = strbuild2.ToString();
 
-        } 
+
+            TreeNode node = MakeTreeFromPaths(pathx, "ROOT", '\\');
+            treeView1.Nodes.Add(node);
+        }
+
+        public TreeNode MakeTreeFromPaths(List<string> paths, string rootNodeName = "", char separator = '/')
+        {
+            var rootNode = new TreeNode(rootNodeName) { ImageIndex = 0 };
+            foreach (var path in paths.Where(x => !string.IsNullOrEmpty(x.Trim())))
+            {
+                var currentNode = rootNode;
+                var pathItems = path.Split(separator);
+                foreach (var item in pathItems)
+                {
+                    var tmp = currentNode.Nodes.Cast<TreeNode>().Where(x => x.Text.Equals(item));
+                    currentNode = tmp.Count() > 0 ? tmp.Single() : currentNode.Nodes.Add(item);
+                }
+            }
+            return rootNode;
+        }
     }
 }
